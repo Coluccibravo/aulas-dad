a86307f1da663fa7df4b7a8688e71503020f6db3
Daniels <cl201117@g.unicamp.br>
2024-04-18 09:19:56 -0300

    ola

--- Dados/Fornecedor.cs
+++ Dados/Fornecedor.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Dados
 {
-    public class Cliente
+    class Fornecedor
     {
         private int id;
-        private TipoPessoa tipoCliente;
+        private TipoPessoa tipoFornecedor;
         private string cpf_cnpj;
         private string razaosocial;
-        private DateTime datanascimento;
         private string nome;
         private string rua;
         private int numero;
@@ -21,27 +20,25 @@ namespace Dados
         private string complemento;
         private string cep;
         private string telefone;
-        private string email;
         private string celular;
-        private decimal limite;
+        private string email;
 
-        public Cliente()
+        public Fornecedor()
         {
         }
 
-        public Cliente(int id, TipoPessoa tipoCliente, string cpf_cnpj, string razaosocial)
+        public Fornecedor(int id, TipoPessoa tipoFornecedor, string cpf_cnpj, string razaosocial)
         {
             this.Id = id;
-            this.TipoCliente = tipoCliente;
+            this.TipoFornecedor = tipoFornecedor;
             this.Cpf_cnpj = cpf_cnpj;
             this.Razaosocial = razaosocial;
         }
 
         public int Id { get => id; set => id = value; }
-        public TipoPessoa TipoCliente { get => tipoCliente; set => tipoCliente = value; }
+        public TipoPessoa TipoFornecedor { get => tipoFornecedor; set => tipoFornecedor = value; }
         public string Cpf_cnpj { get => cpf_cnpj; set => cpf_cnpj = value; }
         public string Razaosocial { get => razaosocial; set => razaosocial = value; }
-        public DateTime Datanascimento { get => datanascimento; set => datanascimento = value; }
         public string Nome { get => nome; set => nome = value; }
         public string Rua { get => rua; set => rua = value; }
         public int Numero { get => numero; set => numero = value; }
@@ -50,8 +47,7 @@ namespace Dados
         public string Complemento { get => complemento; set => complemento = value; }
         public string Cep { get => cep; set => cep = value; }
         public string Telefone { get => telefone; set => telefone = value; }
-        public string Email { get => email; set => email = value; }
         public string Celular { get => celular; set => celular = value; }
-        public decimal Limite { get => limite; set => limite = value; }
+        public string Email { get => email; set => email = value; }
     }
 }
---
+++ Dados/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Produto
+    {
+        private int id;
+        private string nome;
+        private decimal preco;
+
+        public Produto()
+        {
+        }
+
+        public Produto(int id, string nome, decimal preco)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public decimal Preco { get => preco; set => preco = value; }
+    }
+}
